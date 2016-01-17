14c27a13c7fb2dc384ef9b644fa5cb6d9eaa73a3
ssapq@sina.com <ssapq@sina.com>
2016-01-17 17:38:12 +0800

    采集器第一版本 基于Storm

---
+++ crawler/src/main/java/core/Start.java
@@ -0,0 +1,7 @@
+package core;
+
+/**
+ * Created by ss on 2016/1/17.
+ */
+public class Start {
+}
---
+++ crawler/src/main/java/model/Task.java
@@ -0,0 +1,36 @@
+package model;
+
+/**
+ * Created by ss on 2016/1/17.
+ * 采集任务
+ */
+public class Task {
+
+    private Long id;
+    private Integer type;
+    private String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
